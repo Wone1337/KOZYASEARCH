9279c8be3dd48ffe9827059a74fd4115414fb715
wone1337 <stepan.moiseev.2018@bk.ru>
2024-06-03 12:45:32 -0400

    first commit

---
+++ kozyasearch_app/cmake-build/CMakeFiles/KOZYA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KOZYA.dir/main.cpp.o"
+  "CMakeFiles/KOZYA.dir/main.cpp.o.d"
+  "KOZYA"
+  "KOZYA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KOZYA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
